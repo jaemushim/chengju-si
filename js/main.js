208db4ddda7ff4d655e5d334f02d7997d4877f02
jaemushim <shimwoan@gmail.com>
2020-11-22 04:19:20 +0900

    ing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Top popup
+//상단 팝업창
 function getCookie(cname) {
   var name = cname + "=";
   var ca = document.cookie.split(";");
@@ -35,23 +35,22 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  // PC Language Drop Down
+  // 언어 드롭다운
   $(".lang__link").click(function () {
     $(".lang-list").slideToggle("fast");
   });
 
-  // PC Menu Hover
-
-  $(".dept1__item-link").hover(
+  // PC 메뉴 HOVER
+  $(".dept-1__item-link").hover(
     function (e) {
       if ($(window).width() >= 1024) {
-        $(".dept1-wrap").removeClass("m-menu-active");
+        $(".dept-1-wrap").removeClass("m-menu-active");
 
         let dataValue = $(this).data("order");
-        let selector = `.dept2-wrap[data-order=${dataValue}]`;
+        let selector = `.dept-2-wrap[data-order=${dataValue}]`;
 
         $(selector).css("display", "block");
-        $(this).addClass("dept1__item-link--active");
+        $(this).addClass("dept-1__item-link--active");
 
         $(selector).hover(
           function () {
@@ -66,58 +65,58 @@ $(document).ready(function () {
     function (e) {
       if ($(window).width() >= 1024) {
         let dataValue = $(this).data("order");
-        let selector = `.dept2-wrap[data-order=${dataValue}]`;
+        let selector = `.dept-2-wrap[data-order=${dataValue}]`;
 
         $(selector).css("display", "none");
-        $(this).removeClass("dept1__item-link--active");
+        $(this).removeClass("dept-1__item-link--active");
       }
     }
   );
 
-  // Mobile Hamberger toggle
+  // 모바일 햄버거 아이콘 토글
   $(".m-hamberger").click(function () {
-    $(".dept2-wrap").css("display", "none");
+    $(".dept-2-wrap").css("display", "none");
 
-    $(".dept1-wrap").toggleClass("m-menu-active");
+    $(".dept-1-wrap").toggleClass("m-menu-active");
     $(".m-shade").toggleClass("m-shade-active");
     $(".down-arrow").toggleClass("none");
   });
-  //shade
+  // 모바일 메뉴 그림자
   $(".m-shade").click(function () {
-    $(".dept2-wrap").css("display", "none");
+    $(".dept-2-wrap").css("display", "none");
 
     $(".m-shade").removeClass("m-shade-active");
-    $(".dept1-wrap").removeClass("m-menu-active");
+    $(".dept-1-wrap").removeClass("m-menu-active");
     $(".down-arrow").removeClass("none");
   });
-  //m-menu-close
+  //모바일 메뉴 x버튼 클릭
   $(".m-menu-close").click(function () {
-    $(".dept2-wrap").css("display", "none");
+    $(".dept-2-wrap").css("display", "none");
 
     $(".m-shade").removeClass("m-shade-active");
-    $(".dept1-wrap").removeClass("m-menu-active");
+    $(".dept-1-wrap").removeClass("m-menu-active");
     $(".down-arrow").removeClass("none");
   });
 
-  // Mobile Menu Drop Down (dept-1)
-  $(".dept1__item-link").click(function (e) {
+  // 모바일메뉴 드롭다운 (dept-1)
+  $(".dept-1__item-link").click(function (e) {
     if ($(window).width() < 1024) {
       let dataValue = $(this).data("order");
-      let selector = `.dept2-wrap[data-order=${dataValue}]`;
+      let selector = `.dept-2-wrap[data-order=${dataValue}]`;
 
-      $(".dept2-wrap").not(selector).slideUp("fast");
-      $(".dept3-wrap").not(selector).slideUp("fast");
+      $(".dept-2-wrap").not(selector).slideUp("fast");
+      $(".dept-3-wrap").not(selector).slideUp("fast");
 
       $(selector).slideToggle("fast");
     }
   });
-  // Mobile Menu Drop Down (dept-2)
-  $(".dept2__item-link").click(function (e) {
+  // 모바일메뉴 드롭다운 (dept-2)
+  $(".dept-2__item-link").click(function (e) {
     if ($(window).width() < 1024) {
       let dataValue = $(this).data("order");
-      let selector = `.dept3-wrap[data-order=${dataValue}]`;
+      let selector = `.dept-3-wrap[data-order=${dataValue}]`;
 
-      $(".dept3-wrap").not(selector).slideUp("fast");
+      $(".dept-3-wrap").not(selector).slideUp("fast");
 
       $(selector).slideToggle("fast");
     }
@@ -125,9 +124,224 @@ $(document).ready(function () {
 });
 
 $(window).on("resize", function () {
-  var win = $(this); //this = window
+  var win = $(this);
   if (win.width() >= 1024) {
-    $(".dept2-wrap").css("display", "none");
-    $(".dept2-wrap").removeClass("dept1__item-link--active");
+    $(".dept-2-wrap").css("display", "none");
+    $(".dept-2-wrap").removeClass("dept-1__item-link--active");
   }
 });
+
+// 메인화면 상단 섹션 이미지 슬라이더 (슬릭 X)
+mainSlider();
+function mainSlider() {
+  var slideIndex = 1;
+
+  function plusSlides(n) {
+    $(".slider-sec .slider").removeClass("active");
+    showSlides(slideIndex + n);
+    slideIndex += n;
+  }
+
+  function showSlides(n) {
+    var i;
+    var output = n;
+    var slides = document.getElementsByClassName("slider-sec__slide");
+    if (n > slides.length) {
+      output = 1;
+      slideIndex = output - 1;
+    }
+    if (n < 1) {
+      output = slides.length;
+      slideIndex = output + 1;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[output - 1].style.display = "block";
+  }
+
+  showSlidesAuto(slideIndex);
+
+  function showSlidesAuto(order) {
+    if ($(" .slider-sec .slider").hasClass("active")) {
+      var i;
+      var slides = document.getElementsByClassName("slider-sec__slide");
+      if (order > slides.length) {
+        slideIndex = 1;
+      }
+      if (order < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[slideIndex - 1].style.display = "block";
+      function increaseIndex() {
+        if ($(" .slider-sec .slider").hasClass("active")) {
+          slideIndex += 1;
+        }
+      }
+      setTimeout(function () {
+        increaseIndex();
+        showSlidesAuto(slideIndex);
+      }, 5000);
+    }
+  }
+  mainSlider.plusSlides = plusSlides;
+}
+//
+$(document).ready(function () {
+  // 자주찾는 메뉴 섹션 -  슬라이더 (Slick)
+  $(".quick-menu-sec .slick-slider").slick({
+    infinite: true,
+    rows: 1,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 3,
+          rows: 2,
+        },
+      },
+    ],
+  });
+
+  // 소식 섹션 - 팝업 슬라이더 (Slick)
+  $(".news-sec .popup-slider").slick({
+    infinite: true,
+    rows: 1,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {},
+      },
+    ],
+  });
+
+  //민원 및 소통 섹션 - 아이콘 슬라이더
+  $complaintIconPrev = document.querySelector(
+    ".complaint-sec .row .slick-prev"
+  );
+  $complaintIconNext = document.querySelector(
+    ".complaint-sec .row .slick-next"
+  );
+
+  $(".icon-grid").slick({
+    infinite: true,
+    rows: 1,
+    slidesToScroll: 1,
+    slidesToShow: 5,
+    prevArrow: $complaintIconPrev,
+    nextArrow: $complaintIconNext,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          rows: 1,
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          rows: 1,
+        },
+      },
+    ],
+  });
+  //민원 및 소통 섹션 - 미니 배너 슬라이더
+  $(".banner-group").slick({
+    infinite: true,
+    rows: 1,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {},
+      },
+    ],
+  });
+});
+
+// 소식 섹션 - 탭 메뉴
+function openTab(evt, contentName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".news-sec .tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll(".news-sec .tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(contentName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// 민원 및 소통 섹션 - 탭 메뉴
+$complaintSnsPrev = document.querySelector(".complaint-sec .row-2 .slick-prev");
+$complaintSnsNext = document.querySelector(".complaint-sec .row-2 .slick-next");
+
+var socialSlickOption = {
+  infinite: true,
+  rows: 1,
+  slidesToScroll: 1,
+  slidesToShow: 3,
+  prevArrow: $complaintSnsPrev,
+  nextArrow: $complaintSnsNext,
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {},
+    },
+  ],
+};
+$(".complaint-sec .sns-wrap").slick(socialSlickOption);
+
+function openTab2(evt, contentName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".complaint-sec .tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll(".complaint-sec .tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(contentName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  //민원 및 소통 섹션 - SNS 슬라이더
+  $("." + contentName).slick("unslick");
+  $("." + contentName).slick(socialSlickOption);
+}
+// -- 탭메뉴 defaultOpen
+document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen2").click();
